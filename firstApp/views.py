8566e3db69dc5713cd2e4cc10a5ec3db64fffe1a
tony <tony@computer.com>
2023-02-20 07:57:47 +0100

    updated views.py
    added an item display along with the name of todolsit

--- firstApp/views.py
+++ firstApp/views.py
@@ -10,7 +10,8 @@ def index(response, id):
 
 def get_by_name(response, name):
     Todo = Todolist.objects.get(name=name)
-    return HttpResponse("<h1>{}</h1>".format(Todo.name))
+    Todo_items = Todo.item_set.all()[0]
+    return HttpResponse("<h1>{}</h1><br><p>{}</p>".format(Todo.name, str(Todo_items.text)))
 
 def v1(response):
     return HttpResponse("<h1>Version 1!<h1>")
